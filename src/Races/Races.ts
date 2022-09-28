eecbc51324aa7382f76bed2d65ebfbba040d62aa
lucas <fernandes20lucas@gmail.com>
2022-09-28 16:27:56 -0300

    feat: create class Race and completed req01

---
+++ src/Races/Races.ts
@@ -0,0 +1,17 @@
+abstract class Race {
+  readonly name: string;
+  readonly dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this.name = name;
+    this.dexterity = dexterity;
+  }
+
+  public static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  public abstract get maxLifePoints(): number;
+}
+
+export default Race;
